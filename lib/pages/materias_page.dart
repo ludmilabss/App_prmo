90067a6811771acb968b67ec3570742b339f6610
antonyg5 <104701006+antonyg5@users.noreply.github.com>
2022-08-01 15:26:36 -0300

    Update materias_page.dart

--- lib/pages/materias_page.dart
+++ lib/pages/materias_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'monitor_page.dart';
 
 class MateriasPage extends StatefulWidget {
@@ -14,13 +13,7 @@ class _MateriasPageState extends State<MateriasPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: GestureDetector(
-          onTap: () {},
-          child: const Icon(
-            Icons.menu,
-            color: Colors.black,
-          ),
-        ),
+        leading: BackButton(color: Colors.black),
         backgroundColor: Colors.white,
         title: const Text(
           "MATÉRIAS",
@@ -45,118 +38,126 @@ class _MateriasPageState extends State<MateriasPage> {
               child: Row(children: [
                 Expanded(
                     child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: const [
-                    Text("TURMA 911 - Matérias com monitoria disponível",
-                        style: TextStyle(
-                            fontFamily: 'Roboto',
-                            fontSize: 23,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white)),
-                    SizedBox(height: 4),
-                    Text(
-                      "Informática - Matutino",
-                      style: TextStyle(
-                          fontFamily: 'Roboto',
-                          fontSize: 19,
-                          color: Colors.white),
-                    )
-                  ],
-                ))
+
+                      children: const [
+                        Text("TURMA 913 - Matérias com monitoria disponível",
+                            style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontSize: 23,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white)),
+                        SizedBox(height: 4),
+                        Text(
+                          "Informática - Matutino",
+                          style: TextStyle(
+                              fontFamily: 'Roboto',
+                              fontSize: 19,
+                              color: Colors.white),
+                        )
+                      ],
+                    ))
               ]),
             ),
+
             const SizedBox(height: 24),
-            Column(
-              children: [
-                const Text("Técnicas em Informática",
-                    style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontSize: 23,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF6BC07D),
-                      decoration: TextDecoration.underline,
-                    )),
-                const SizedBox(height: 24),
-                buildMaterias(
-                    materia: "PWEB",
-                    left: 103.0,
-                    right: 103.0,
-                    cor: 0xFFFFEBEB),
-                const SizedBox(height: 4),
-                buildMaterias(
-                    materia: "PRMO",
-                    left: 102.0,
-                    right: 102.0,
-                    cor: 0xFFFFEBEB),
-                const SizedBox(height: 4),
-                buildMaterias(
-                    materia: "ISRE",
-                    left: 110.0,
-                    right: 110.0,
-                    cor: 0xFFFFEBEB),
-                const SizedBox(height: 24),
-                const Text("Ensino Médio",
-                    style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontSize: 23,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF6BC07D),
-                      decoration: TextDecoration.underline,
-                    )),
-                const SizedBox(height: 24),
-                buildMaterias(
-                    materia: "Biologia",
-                    left: 96.0,
-                    right: 96.0,
-                    cor: 0xFFFFE3B9),
-                const SizedBox(height: 4),
-                buildMaterias(
-                    materia: "Química",
-                    left: 96.0,
-                    right: 96.0,
-                    cor: 0xFFFFE3B9),
-                const SizedBox(height: 4),
-                buildMaterias(
-                    materia: "Português",
-                    left: 86.0,
-                    right: 86.0,
-                    cor: 0xFFFFE3B9),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Text("Técnicas em Informática",
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF6BC07D),
+                        decoration: TextDecoration.underline,
+                      )
+                  ),
+                  const SizedBox(height: 24),
+                  buildMaterias(
+                      materia: "PWEB",
+                      left: 103.0,
+                      right: 103.0,
+                      cor: 0xFFFFEBEB),
+
+                  const SizedBox(height: 4),
+                  buildMaterias(
+                      materia: "PRMO",
+                      left: 102.0,
+                      right: 102.0,
+                      cor: 0xFFFFEBEB),
+
+                  const SizedBox(height: 4),
+                  buildMaterias(
+                      materia: "ISRE",
+                      left: 110.0,
+                      right: 110.0,
+                      cor: 0xFFFFEBEB),
+
+                  const SizedBox(height: 24),
+                  const Text("Ensino Médio",
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF6BC07D),
+                        decoration: TextDecoration.underline,
+                      )
+                  ),
+                  const SizedBox(height: 24),
+                  buildMaterias(
+                      materia: "Biologia",
+                      left: 96.0,
+                      right: 96.0,
+                      cor: 0xFFFFE3B9),
+
+                  const SizedBox(height: 4),
+                  buildMaterias(
+                      materia: "Química",
+                      left: 96.0,
+                      right: 96.0,
+                      cor: 0xFFFFE3B9),
+
+                  const SizedBox(height: 4),
+                  buildMaterias(
+                      materia: "Português",
+                      left: 86.0,
+                      right: 86.0,
+                      cor: 0xFFFFE3B9),
+                ],
+              ),
             ),
           ],
         ),
       ),
     );
   }
-
   buildMaterias({
     required String materia,
     required double left,
     required double right,
     required int cor,
-  }) {
+  }){
     return ElevatedButton(
         onPressed: onPressedButn,
+        child: Text(
+          materia,
+          style: const TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
         style: ElevatedButton.styleFrom(
           primary: Color(cor),
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(32.0)),
-        ),
-        child: Padding(
-          padding:
-              EdgeInsets.only(left: left, right: right, bottom: 8.0, top: 8.0),
-          child: Text(
-            materia,
-            style: const TextStyle(
-              fontFamily: 'Roboto',
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(32.0)
           ),
-        ));
+        )
+    );
   }
-
   void onPressedButn() {
     Navigator.push(
       context,
