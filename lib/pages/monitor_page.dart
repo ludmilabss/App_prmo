82a553c97b69facba735f1f994f7c41002a909c1
antonyg5 <104701006+antonyg5@users.noreply.github.com>
2022-08-01 15:30:56 -0300

    Update monitor_page.dart

--- lib/pages/monitor_page.dart
+++ lib/pages/monitor_page.dart
@@ -22,8 +22,7 @@ class _TelaPageState extends State<TelaPage> {
         backgroundColor: Colors.white,
         title: const Text(
           'PROGRAMAÇÃO WEB',
-          style: TextStyle(
-            fontSize: 24,
+          style: TextStyle(fontSize: 24,
             color: Colors.black,
             fontWeight: FontWeight.bold,
           ),
@@ -43,13 +42,11 @@ class _TelaPageState extends State<TelaPage> {
                         fontWeight: FontWeight.bold,
                         color: Colors.black)),
                 const SizedBox(height: 16),
-                buildMonitor(
-                    nome: 'GIANCARLO LÚCIO DO NASCIMENTO',
+                buildMonitor(nome: 'GIANCARLO LÚCIO DO NASCIMENTO',
                     email: 'gln1@aluno.ifal.edu.br',
                     url: 'https://cdn-icons-png.flaticon.com/512/74/74472.png'),
                 const SizedBox(height: 16),
-                buildMonitor(
-                    nome: 'MARIA EDUARDA BARBOSA DE LIMA',
+                buildMonitor(nome: 'MARIA EDUARDA BARBOSA DE LIMA',
                     email: 'mebl2@aluno.ifal.edu.br',
                     url: 'https://cdn-icons-png.flaticon.com/512/74/74472.png'),
                 const SizedBox(height: 20),
@@ -72,13 +69,11 @@ class _TelaPageState extends State<TelaPage> {
                   ],
                 ),
                 const SizedBox(height: 20),
-                buildLocal(
-                    data: 'Hoje, 23/06',
+                buildLocal(data: 'Hoje, 21/06',
                     sala: 'LABORATÓRIO 91 - DESENVOLVIMENTO DE SOFTWARE',
                     horario: '14:00 - 16:00'),
                 const SizedBox(height: 16),
-                buildLocal(
-                    data: 'Amanhã, 24/06',
+                buildLocal(data: 'Amanhã, 22/06',
                     sala: 'LABORATÓRIO 90 - DESENVOLVIMENTO DE SOFTWARE',
                     horario: '14:00 - 17:00'),
                 const SizedBox(height: 20),
@@ -101,35 +96,34 @@ class _TelaPageState extends State<TelaPage> {
         borderRadius: BorderRadius.circular(16),
       ),
       padding: const EdgeInsets.all(16.0),
-      child: Row(children: [
-        Expanded(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+      child: Row(
           children: [
-            Text(nome,
-                style: const TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 21,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white)),
-            const SizedBox(height: 10),
-            Text(
-              email,
-              style: const TextStyle(
-                  fontFamily: 'Roboto', fontSize: 17, color: Colors.white),
-            )
-          ],
-        )),
-        Row(
-          children: [
-            Image.network(
-              url,
+            Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                        nome,
+                        style: const TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 21,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white)),
+                    const SizedBox(height: 10),
+                    Text(
+                      email,
+                      style: const TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 17,
+                          color: Colors.white),
+                    )
+                  ],
+                )),
+            Image.network(url,
               height: 100,
               width: 100,
             ),
-          ],
-        ),
-      ]),
+          ]),
     );
   }
 
@@ -147,29 +141,37 @@ class _TelaPageState extends State<TelaPage> {
       child: Row(children: [
         Expanded(
             child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(data,
-                style: const TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black)),
-            const SizedBox(height: 15),
-            Text(
-              sala,
-              style: const TextStyle(
-                  fontFamily: 'Roboto', fontSize: 18, color: Colors.black),
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                    data,
+                    style: const TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black)),
+                const SizedBox(height: 15),
+                Text(
+                  sala,
+                  style: const TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 18,
+                      color: Colors.black),
+                ),
+                const SizedBox(height: 15),
+                Text(
+                  horario,
+                  style: const TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 18,
+                      color: Colors.black),
+                ),
+              ],
             ),
-            const SizedBox(height: 15),
-            Text(
-              horario,
-              style: const TextStyle(
-                  fontFamily: 'Roboto', fontSize: 18, color: Colors.black),
-            ),
-          ],
-        )),
-      ]),
+        ),
+      ],
+      ),
     );
   }
+
 }
