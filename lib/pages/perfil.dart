571119b25599a7e2d45717065ad88b2a7e4076c3
antonyg5 <104701006+antonyg5@users.noreply.github.com>
2022-08-24 15:53:15 -0300

    Add files via upload

---
+++ lib/pages/perfil.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PerfilPage extends StatefulWidget {
+  const PerfilPage({Key? key}) : super(key: key);
+
+  @override
+  State<PerfilPage> createState() => _PerfilPageState();
+}
+
+class _PerfilPageState extends State<PerfilPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: const [
+          Text("data"),
+        ],
+      ),
+    );
+  }
+}
