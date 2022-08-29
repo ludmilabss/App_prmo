c25e6e26e6d7ec54c46713e40935b36475b9a1a4
aBeActress <103438080+aBeActress@users.noreply.github.com>
2022-08-29 10:31:16 -0300

    Create calendario_page.dart

---
+++ lib/pages/calendario_page.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: [const Locale('pt', 'BR')],
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  CalendarController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = CalendarController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+          onTap: () {},
+          child: const Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        title: const Text(
+          'CALENDÁRIO',
+          style: TextStyle(fontSize: 24,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                const SizedBox(height: 8.0),
+                SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      TableCalendar(
+                        initialCalendarFormat: CalendarFormat.month,
+                        calendarStyle: CalendarStyle(
+                            todayColor: Color(0xFF6BC07D),
+                            selectedColor: Theme.of(context).primaryColor,
+                            todayStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18.0,
+                                color: Colors.white)),
+                        headerStyle: HeaderStyle(
+                          centerHeaderTitle: true,
+                          formatButtonDecoration: BoxDecoration(
+                            color: Color(0xFF6BC07D),
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          formatButtonTextStyle: TextStyle(color: Colors.white),
+                          formatButtonShowsNext: false,
+                        ),
+                        availableCalendarFormats: const{
+                          CalendarFormat.month: "Mês",
+                          CalendarFormat.week: "Semana",
+                          CalendarFormat.twoWeeks: "2 semanas",
+                        },
+                        startingDayOfWeek: StartingDayOfWeek.sunday,
+                        onDaySelected: (date, events, _) {
+                          print(date.toIso8601String());
+                        },
+                        builders: CalendarBuilders(
+                          selectedDayBuilder: (context, date, events) => Container(
+                              margin: const EdgeInsets.all(4.0),
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColor,
+                                  borderRadius: BorderRadius.circular(10.0)),
+                              child: Text(
+                                date.day.toString(),
+                                style: TextStyle(color: Colors.white),
+                              )),
+                          todayDayBuilder: (context, date, events) => Container(
+                              margin: const EdgeInsets.all(4.0),
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                  color: Color(0xFF6BC07D),
+                                  borderRadius: BorderRadius.circular(10.0)),
+                              child: Text(
+                                date.day.toString(),
+                                style: TextStyle(color: Colors.white),
+                              )),
+                        ),
+                        calendarController: _controller,
+                      )
+                    ],
+                  ),
+                ),
+              ],
+              //inserir aqui os placeholders para sinalizar os eventos
+
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
