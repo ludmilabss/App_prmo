a4d19691fd7e5affe1bed2ea053c9e8c6650cef4
antonyg5 <104701006+antonyg5@users.noreply.github.com>
2022-08-30 07:32:46 -0300

    Add files via upload

---
+++ lib/domain/btn_monitor.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class BtnMonitor {
+ final String text;
+  final IconData icone;
+  final Widget classe;
+
+ BtnMonitor({
+   required this.text,
+  required this.icone,
+  required this.classe,
+ });
+}
