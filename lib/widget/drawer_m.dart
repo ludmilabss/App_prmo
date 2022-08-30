3bdc6a295ff97d4c6a32bff8c107488db0924832
antonyg5 <104701006+antonyg5@users.noreply.github.com>
2022-08-30 12:02:11 -0300

    Add files via upload

---
+++ lib/widget/drawer_m.dart
@@ -0,0 +1,158 @@
+import 'package:app_prmo/monitor_pages/home_monitor.dart';
+import 'package:app_prmo/pages/perfil_page.dart';
+import 'package:flutter/material.dart';
+import '../pages/atendimento_page.dart';
+import '../pages/calendario_page.dart';
+
+
+class DrawerWidget1 extends StatefulWidget {
+  const DrawerWidget1({Key? key}) : super(key: key);
+
+  @override
+  State<DrawerWidget1> createState() => _DrawerWidget1State();
+}
+
+class _DrawerWidget1State extends State<DrawerWidget1> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+
+      child: ListView(
+        children: [
+          const UserAccountsDrawerHeader(
+            accountName: Text(
+              'Beltrano Ferreira',
+              style: TextStyle(
+                color: Color(0xFFFFFFFF),
+                fontFamily: 'Roboto',
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            accountEmail: Text('bf1@aluno.ifal.edu.br'),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Color(0xFFD9D9D9),
+              child: Text(
+                'BF',
+                style: TextStyle(
+                  color: Color(0xFFFFFFFF),
+                  fontFamily: 'Roboto',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            decoration: BoxDecoration(color: Color(0xFF6BC07D)),
+          ),
+          ListTile(
+            title: const Text(
+              "Home",
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Roboto',
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            trailing: const Icon(Icons.house_outlined),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) => const HomeMonitor()));
+            },
+          ),
+          Divider(),
+          ListTile(
+            title: const Text(
+              "Perfil",
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Roboto',
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            trailing: Icon(Icons.person),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) => const PerfilPage()));
+            },
+          ),
+          const Divider(),
+          ListTile(
+            title: const Text(
+              "Atendimento",
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Roboto',
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            trailing: Icon(Icons.group),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) => AtendimentoPage()));
+            },
+          ),
+          const Divider(),
+          ListTile(
+            title: const Text(
+              "Mural",
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Roboto',
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            trailing: Icon(Icons.chat_outlined),
+            onTap: () {
+              // Navigator.of(context).pop();
+              // Navigator.of(context).push(MaterialPageRoute(
+              //     builder: (BuildContext context) => const MateriasPage()));
+            },
+          ),
+          const Divider(),
+          ListTile(
+            title: const Text(
+              "Calendário",
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Roboto',
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            trailing: const Icon(Icons.calendar_month),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) => CalendarioPage()));
+            },
+          ),
+          const Divider(),
+          ListTile(
+            title: const Text(
+              "Frequência",
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Roboto',
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            trailing: const Icon(Icons.assignment_outlined),
+            onTap: () {
+              // Navigator.of(context).pop();
+              // Navigator.of(context).push(MaterialPageRoute(
+              //     builder: (BuildContext context) => CalendarioPage()));
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
