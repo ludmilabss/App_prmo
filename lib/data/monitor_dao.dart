14ae94b5ffc91400b09e2cfd8f319da3754a47e3
guilherme-gjv <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>
2022-10-18 09:34:30 -0300

    chore: função de criar

--- lib/data/monitor_dao.dart
+++ lib/data/monitor_dao.dart
@@ -1,8 +1,10 @@
 import 'package:app_prmo/data/db_helper.dart';
+import 'package:app_prmo/domain/monitor.dart';
 import 'package:sqflite/sqflite.dart';
 
-class MonitorDao{
-  Future<bool> autenticar({required String email, required String password}) async{
+class MonitorDao {
+  Future<bool> autenticar(
+      {required String email, required String password}) async {
     DBHelper dbHelper = DBHelper();
     Database db = await dbHelper.initDB();
 
@@ -10,5 +12,26 @@ class MonitorDao{
     final result = await db.rawQuery(sql, [email, password]);
 
     return result.isNotEmpty;
+  }
+
+  Future<Monitor> criar(
+      {required String email,
+      required String password,
+      required String name,
+      required bool monitor}) async {
+    Monitor monitorAux =
+        Monitor(email: email, password: password, name: name, monitor: monitor);
+    DBHelper dbHelper = DBHelper();
+    Database db = await dbHelper.initDB();
+
+    String sql =
+        "INSERT INTO MONITOR (id, name, email, password, monitor) VALUES (DEFAULT, ?, ?, ?, ?);";
+    await db.insert(
+      'MONITOR',
+      monitorAux.toJson(),
+    );
+    // await db.execute(sql);
+
+    return monitorAux;
   }
 }
