6b72ab2fe1df0263e63895eded730bce909fdb2c
CamileV <cvfa2@aluno.ifal.edu.br>
2022-10-25 08:35:59 -0300

    Atualização teste

---
+++ lib/data/turma_dao.dart
@@ -0,0 +1,22 @@
+import 'package:app_prmo/domain/turmas.dart';
+import 'package:sqflite/sqflite.dart';
+
+import 'db_helper.dart';
+
+class TurmaDao{
+  Future<List<Turmas>> listarTurmas() async{
+    DBHelper dbHelper = DBHelper();
+    Database db = await dbHelper.initDB();
+
+    String sql = "SELECT * FROM TURMAS";
+    final result = await db.rawQuery(sql);
+
+    List<Turmas> lista = <Turmas>[];
+    for(var json in result){
+      Turmas turma = Turmas.fromJson(json);
+      lista.add(turma);
+    }
+
+    return lista;
+  }
+}
