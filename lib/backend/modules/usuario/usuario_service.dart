eae90625518751b4b830fa2c745db042121333b9
guilherme-gjv <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>
2022-10-31 20:18:03 -0300

    aplicando usuario validator no service

--- lib/backend/modules/usuario/usuario_service.dart
+++ lib/backend/modules/usuario/usuario_service.dart
@@ -1,3 +1,5 @@
+import 'package:app_prmo/backend/modules/usuario/validators/usuario_validator.dart';
+
 import 'repository/interface/interface_usuario_repository.dart';
 import 'usuario.dart';
 
@@ -7,16 +9,33 @@ class UsuarioService {
   UsuarioService(this.usuarioRepository);
 
   Future<void> atualizar({required String id, required Usuario usuario}) async {
+    try {
+      UsuarioValidator().fullUsuarioValidator(usuario: usuario);
+    } catch (e) {
+      rethrow;
+    }
     await usuarioRepository.atualizar(id: id, updatedUsuario: usuario);
   }
 
   Future<bool> autenticar({required String email, required String password}) {
+    try {
+      UsuarioValidator().emailValidator(email: email);
+      UsuarioValidator().passwordValidator(password: password);
+    } catch (e) {
+      rethrow;
+    }
+
     final result =
         usuarioRepository.autenticar(email: email, password: password);
     return result;
   }
 
   Future<void> criar({required Usuario usuario}) async {
+    try {
+      UsuarioValidator().fullUsuarioValidator(usuario: usuario);
+    } catch (e) {
+      rethrow;
+    }
     await usuarioRepository.criar(usuario: usuario);
   }
 
