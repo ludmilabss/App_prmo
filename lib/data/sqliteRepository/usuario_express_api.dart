118b5de0e4873ce8ac7066e5733985aabf1f75cc
guilherme-gjv <guijesus.info@gmail.com>
2022-12-16 20:55:19 -0300

    feat: usuario express repository

---
+++ lib/data/sqliteRepository/usuario_express_api.dart
@@ -0,0 +1,100 @@
+import 'dart:convert';
+
+import 'package:app_prmo/data/db_helper.dart';
+import 'package:app_prmo/domain/usuario.dart';
+import 'package:sqflite/sqflite.dart';
+
+import '../interfaces/interface_usuario_repository.dart';
+import 'package:http/http.dart' as http;
+
+class UsuarioSQLiteRepository implements IUsuarioRepository {
+
+  final baseUrl = "10.0.2.2";
+
+  @override
+  Future<void> atualizar(
+      {required Usuario updatedUsuario, required String id}) async {
+    try {
+      final url = Uri.http(baseUrl, "users");
+      await http.patch(url, body: updatedUsuario.toJson());
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+
+  @override
+  Future<bool> autenticar(
+      {required String email, required String password}) async {
+    try {
+      DBHelper dbHelper = DBHelper();
+      Database db = await dbHelper.initDB();
+
+      String sql = 'SELECT * FROM usuarios WHERE email = ? AND password = ?;';
+      final result = await db.rawQuery(sql, [email, password]);
+
+      return result.isNotEmpty;
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+
+  @override
+  Future<void> criar({required Usuario usuario}) async {
+    try {
+      final url = Uri.http(baseUrl, "users");
+      await http.post(url, body: usuario.toJson());
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+
+  @override
+  Future<void> deletar({required String id}) async {
+    try {
+      final url = Uri.http(baseUrl, "users/${id}");
+      await http.delete(url);
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+
+  @override
+  Future<List<Usuario>> listar() async {
+    try {     
+      final url = Uri.http(baseUrl, "users");
+      var usuariosJsonList = await http.get(url);
+      
+      if(usuariosJsonList.statusCode == 200) {
+        List<Usuario> usuariosList = <Usuario>[];
+        for (var json in jsonDecode(usuariosJsonList.body) ) {
+          Usuario usuario = Usuario.fromJson(json);
+          usuariosList.add(usuario);
+        }
+        return usuariosList;
+      } else {
+        throw Exception("Não foi possível listar usuários.");
+      }
+      
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+
+  @override
+  Future<Usuario> pesquisar({required String id}) async {
+    try {
+      
+      final url = Uri.http(baseUrl, "users/${id}");
+      var usuario = await http.get(url);
+
+      if(usuario.statusCode == 200) {
+        return Usuario.fromJson(jsonDecode(usuario.body)) ;
+      } else {
+        throw Exception("Usuário não encontrado.");
+      }
+
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+}
